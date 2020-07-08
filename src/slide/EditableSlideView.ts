91efe0e9808b2507bd5895c3e1cb7b3b0388d1c6
harogen-net <flashmaker@harogen.net>
2020-07-08 14:15:58 +0900

    textLayer?

--- src/slide/EditableSlideView.ts
+++ src/slide/EditableSlideView.ts
@@ -473,7 +473,7 @@ export class EditableSlideView extends DOMSlideView implements IDroppable {
 				}
 			}
 			if(layer.type == LayerType.TEXT){
-				if(flag & PropFlags.IMG_IMAGEID){
+				if(flag & PropFlags.TXT_TEXT){
 					(tmpLayer as TextLayer).text = (layer as TextLayer).text;
 				}
 			}
