8e431ed745a0b4c90afbe40a4e240a765f0bc655
harogen-net <flashmaker@harogen.net>
2024-05-21 17:45:03 +0900

    feat: Fix bug in FileSelector causing incorrect selection index
    This commit fixes a bug in the FileSelector class that was causing the incorrect selection index to be set when the storage was updated. The bug occurred when the previously selected option no longer existed in the updated storage data. This commit ensures that the correct index is set by selecting the option before the previously selected one if it no longer exists.

--- src/viewController/file/FileSelector.ts
+++ src/viewController/file/FileSelector.ts
@@ -9,30 +9,25 @@ export class FileSelector {
 
 		storage.addEventListener("update", (e: CustomEvent) => {
 			let index = selectObj.prop("selectedIndex");
-			let selectedValue = parseInt(($("select.filename option")[index] as HTMLOptionElement).value);
+			let selectedValue = parseInt(($("select.filename option")[index] as HTMLOptionElement).value) || 0;
 			let initOption = $("select.filename option")[0];
 			selectObj.empty();
 			selectObj.append($(initOption));
 
-			let nextIndex = 0;
-			let found = false;
+			let nextIndex = index;
 			storage.titles.forEach((datum, index2) => {
 				if (datum.id == selectedValue) {
 					nextIndex = index2 + 1;
-					found = true;
 				}
 				selectObj.append(`<option value="${datum.id}">${datum.title}</option>`);
 			});
 
-			if (!found) {
-				nextIndex = index - 1; // If the previously selected option no longer exists, select the one before it
-			}
-
-			if (nextIndex > 0 && nextIndex <= storage.titles.length) {
-				selectObj.prop("selectedIndex", nextIndex);
-			} else {
-				selectObj.prop("selectedIndex", storage.titles.length);
-			}
+			if (nextIndex < 0) nextIndex = 0;
+			if (nextIndex > storage.titles.length) nextIndex = storage.titles.length;
+			selectObj.prop("selectedIndex", nextIndex);
+			
+			let nextValue = ($("select.filename option")[nextIndex] as HTMLOptionElement).value;
+			if (nextValue) storage.load(nextValue);
 		});
 
 		const handleSelectChange = (val: any) => {
