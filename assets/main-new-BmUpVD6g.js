20800cfc24fabe0147aeea2bd9a5cef78580a999
Harunori Uchida <uchida@atrasc.co.jp>
2026-06-29 17:52:55 +0900

    Updates

---
+++ assets/main-new-BmUpVD6g.js
@@ -0,0 +1,1 @@
+import{c as o,r as n,A as t}from"./AppShell-C7iJzJkN.js";console.log("[main-new] module evaluated");const e=document.getElementById("root");if(!e)throw new Error("v3 main-new: #root element not found in index.html");console.log("[main-new] mounting <AppShell /> into #root");o(e).render(n.createElement(t));
